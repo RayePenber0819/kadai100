b9e69fbf43a9f99e618f8cc499ba86a773e24349
RayePenber0819 <okada_rei@ftgroup.co.jp>
2025-12-05 16:28:39 +0900

    課題02未完了03

--- webrick.rb
+++ webrick.rb
@@ -7,6 +7,23 @@ server = WEBrick::HTTPServer.new({
   :Port => 8000
 })
 
+class UsersServlet < WEBrick::HTTPServlet::AbstractServlet
+  def do_GET(request, response)
+    response.status = 200
+    response['Content-Type'] = 'text/plain'
+
+    response.body = <<<EOF
+    <ul>
+      <li>太郎</li>
+      <li>次郎</li>
+      <li>三郎</li>
+    </ul>
+    EOF;
+  end
+end
+
+server.mount('./users', UsersServlet)
+
 trap(:INT){
     server.shutdown
 }
