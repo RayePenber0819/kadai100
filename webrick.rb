160b7eb3c3a907b661afb18199ec258caecc0955
root <root@DPC00030.ftad.local>
2025-12-05 09:03:50 +0900

    first commit

---
+++ webrick.rb
@@ -0,0 +1,14 @@
+ # webrick.rb
+require 'webrick'
+
+server = WEBrick::HTTPServer.new({ 
+  :DocumentRoot => './',
+  :BindAddress => '127.0.0.1',
+  :Port => 8000
+})
+
+trap(:INT){
+    server.shutdown
+}
+
+server.start
