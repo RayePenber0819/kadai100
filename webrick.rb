53ddc65e88d226be0abc8dee90885cbabcc040e0
yffty09 <fujisawa_yuta@ftgroup.co.jp>
2025-12-08 10:53:19 +0900

    kadai03追加 (#3)
    * kadai03追加
    
    * 課題03完了01
    
    ---------
    
    Co-authored-by: RayePenber0819 <okada_rei@ftgroup.co.jp>

--- webrick.rb
+++ webrick.rb
@@ -7,6 +7,16 @@ server = WEBrick::HTTPServer.new({
   :Port => 8000
 })
 
+server.mount_proc("/time") do |req, res|
+  # レスポンス内容を出力
+  time = Time.now
+  jst = Time.at(time, in: "+09:00")
+  body = "<html><body>" + jst.to_s + "</body></html>"
+  res.status = 200
+  res['Content-Type'] = 'text/html'
+  res.body = body
+end
+
 trap(:INT){
     server.shutdown
 }
